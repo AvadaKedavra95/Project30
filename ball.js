5c9fcdc888b5684e67dd095f460d1abdb6eb872d
AvadaKedavra95 <74106210+AvadaKedavra95@users.noreply.github.com>
2020-12-02 12:16:11 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,11 @@
+class Ball extends BaseClass {
+  constructor(x,y){
+    super(x,y,40,40);
+    this.image = loadImage("sprites/polygon.png");
+  }
+
+  display() {
+ 
+    super.display();
+  }
+}
